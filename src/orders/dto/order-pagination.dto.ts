2ceec7c1f474edcb456095c11727b39a6b7e74f9
RicardoNoyaCamacho <83376211+RicardoNoyaCamacho@users.noreply.github.com>
2024-07-18 18:19:34 -0600

    Init Orders MS

---
+++ src/orders/dto/order-pagination.dto.ts
@@ -0,0 +1,15 @@
+import { IsEnum, IsOptional } from 'class-validator';
+import { PaginationDto } from '../../common/dtos/pagination.dto';
+import { OrderStatusList } from '../enum/order.enum';
+import { OrderStatus } from '@prisma/client';
+
+
+export class OrderPaginationDto extends PaginationDto {
+
+    @IsOptional()
+    @IsEnum(OrderStatusList, {
+        message: `Valid status are ${OrderStatusList}`
+    })
+    status: OrderStatus;
+
+}
